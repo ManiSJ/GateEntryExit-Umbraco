6ed345472c8a01428a7a73d57c905dd8310d3cfc
ManiSJ <ManiSJ@users.noreply.github.com>
2024-05-06 14:58:18 +1000

    Working java api endpoint

---
+++ Dtos/GateEmployee/AllGateEmployeeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GateEntryExit_Umbraco.Dtos.GateEmployee
+{
+    public class AllGateEmployeeDto
+    {
+        public List<GateEmployeeDto> Content { get; set; }
+
+        public int TotalPages { get; set; }
+    }
+}
--- Services/Interfaces/IGateEmployeeService.cs
+++ Services/Interfaces/IGateEmployeeService.cs
@@ -1,6 +1,9 @@
-﻿namespace GateEntryExit_Umbraco.Services.Interfaces
+﻿using GateEntryExit_Umbraco.Dtos.GateEmployee;
+
+namespace GateEntryExit_Umbraco.Services.Interfaces
 {
     public interface IGateEmployeeService
     {
+        Task<AllGateEmployeeDto> GetAllAsync(int pageNumber = 0);
     }
 }
